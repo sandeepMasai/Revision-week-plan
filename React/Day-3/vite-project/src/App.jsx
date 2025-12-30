54a0a6a836feb80016f71e642759ad7ea05e6082
Sandeep <sk245444@gmail.com>
2025-12-30 20:48:23 +0530

    add code react

---
+++ React/Day-3/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PasswordValidator from "./components/PasswordValidator";
+import MultiStepRegistration from "./components/MultiStepRegistration";
+
+function App() {
+  return (
+    <div>
+      <PasswordValidator />
+      <MultiStepRegistration />
+    </div>
+  );
+}
+
+export default App;
