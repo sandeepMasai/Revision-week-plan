a84dd355a70606ca3119c123adcd49d7bd1bb88d
Sandeep <sk245444@gmail.com>
2026-01-05 22:05:17 +0530

    add code

---
+++ React/Day-7/Debounce/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Debounce from "./Debounce";
+import Expensive from "./Expensive";
+
+function App() {
+  return (
+    <div>
+      <Debounce />
+      <Expensive />
+    </div>
+  );
+}
+
+export default App;
