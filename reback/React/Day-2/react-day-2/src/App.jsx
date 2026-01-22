9c7a80bcc84b197f8ce799e3671d859e2dc52c12
Sandeep <sk245444@gmail.com>
2026-01-22 18:52:03 +0530

    add

---
+++ reback/React/Day-2/react-day-2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Question1 from "./components/Question1";
+import Question2 from "./components/Question2";
+
+function App() {
+  return (
+    <div>
+      <Question1 />
+
+      <br />
+
+      <Question2 />
+    </div>
+  );
+}
+
+export default App;
